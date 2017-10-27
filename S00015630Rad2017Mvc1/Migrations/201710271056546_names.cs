9233e145396e3312607eeea0a7c36ab2d235b99b
S00015630 <S00015630@mail.itsligo.it>
2017-10-27 12:01:44 +0100

    seeding app,  part 6 done

---
+++ S00015630Rad2017Mvc1/Migrations/201710271056546_names.cs
@@ -0,0 +1,20 @@
+namespace S00015630Rad2017Mvc1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class names : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "FirstName", c => c.String());
+            AddColumn("dbo.AspNetUsers", "Surname", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "Surname");
+            DropColumn("dbo.AspNetUsers", "FirstName");
+        }
+    }
+}
---
+++ S00015630Rad2017Mvc1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(S00015630Rad2017Mvc1.Startup))]
+namespace S00015630Rad2017Mvc1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
